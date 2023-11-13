358537b940a6a626f8fcd57f3ba48b0e71dd6bfc
Александр Родионов <aleksandrrodionov@MacBook-Air-Aleksandr.local>
2023-11-14 01:49:19 +0300

    third-version

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ function secondSliderInit() {
           clickable: true,
       },
     });
-    console.log('я отработал')
   }
 }
 
@@ -48,7 +47,7 @@ window.addEventListener("resize", function() {
 if (window.matchMedia("(max-width: 767px)").matches) {
   sliderInit();
   secondSliderInit();
-} else{
+} else {
   sliderDestroy();
 }
 });
@@ -68,15 +67,80 @@ btn.addEventListener("click", function() {
     container.classList.toggle("swiper--active");
 });
 
-
+const callButton = document.querySelector(".info-bar__button")
 const closeButton = document.querySelector(".modal-close-button")
-btn.addEventListener("click", function(){
-    console.log(closeButton)
-    console.log('эй')
-    btn.classList.toggle("close-button--active")
+const modalWrapper = document.querySelector(".modal")
+const burgerButton = document.querySelector(".menu-button")
+const burgerWrapper = document.querySelector(".sidebar")
+const burgerCloseButton = document.querySelector(".sidebar__button")
+const modalOverlay = document.querySelector(".overlay")
+const links = document.querySelectorAll("sidebar__menu--element")
+const bodyScroll = document.body;
+
+const picture = document.querySelector(".picture-menu")
+
+closeButton.addEventListener("click", function() {
+    modalWrapper.classList.add("modal--closed");
+    bodyScroll.style.overflow = "auto";
+})
+
+callButton.addEventListener("click", function() {
+  
+  modalWrapper.classList.remove("modal--closed");
+  bodyScroll.style.overflow = "hidden";
+})
+
+modalWrapper.addEventListener("click", function(e) {
+  if(e.target === modalOverlay){
+    modalWrapper.classList.toggle("modal--closed");
+    bodyScroll.style.overflow = "auto";
+  }
+
+})
+
+const toggleButtonStyle = () => {
+  const isOpen = burgerWrapper.classList.contains("sidebar--open");
+if (isOpen) {
+  picture.style.background = "#43CD93";
+  picture.style.width = "41px";
+} else {
+  picture.style.background = "#6B5AF9";
+  picture.style.width = "15px";
+}
+}
+
+burgerButton.addEventListener("click", function() {
+  burgerWrapper.classList.toggle("sidebar--open");
+  bodyScroll.style.overflow = "hidden";
+  toggleButtonStyle();
+  burgerWrapper.classList.contains("sidebar--open") ? bodyScroll.style.overflow = "hidden" : bodyScroll.style.overflow = "auto";
+})
+
+burgerCloseButton.addEventListener("click", function(e) {
+  if (e.target === burgerCloseButton){
+    burgerWrapper.classList.remove("sidebar--open");
+    bodyScroll.style.overflow = "auto";
+    toggleButtonStyle();
+  }
+
+})
+
+burgerWrapper.addEventListener("click", function(e) {
+  if (e.target === burgerWrapper){
+    burgerWrapper.classList.remove("sidebar--open");
+    bodyScroll.style.overflow = "auto";
+    toggleButtonStyle();
+  }
 })
 
 
+document.body.addEventListener('click', e => {
+  if (e.target.classList.contains('sidebar__menu--element')) {
+    burgerWrapper.classList.remove("sidebar--open");
+    bodyScroll.style.overflow = "auto";
+    toggleButtonStyle();
+  }
+})
 
 
 
